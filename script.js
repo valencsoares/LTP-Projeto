c37ed324e3708047cb85c3e46833cbbf5e963027
valencsoares <pvcampossoares558@gmail.com>
2023-05-03 17:25:52 -0400

    Colocado informações dos produtos ao HTML.

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ class Produto {
         this.preco = preco
     }
     mostrarProduto(){
-
+        return `
+        <h4>${this.nome}</h4>
+        <p>${this.datadeCadastro}</p>
+        <p>${this.descricao}</p>
+        <p>${this.preco}</p>
+        </br>
+        `
     }
 }
 
@@ -17,12 +23,26 @@ class ProdutoDestaque extends Produto {
         this.imagemDestaque = imagemDestaque
     }
     mostrarProdutoDestaque(){
-        "Nome do produto: " + this.nome + ", Descrição: " + this.descricao + this.imagemDestaque
+        return `
+        <h4>${this.nome}</h4>
+        <p>${this.datadeCadastro}</p>
+        <p>${this.descricao}</p>
+        <p>${this.preco}</p>
+        <p>${this.imagemDestaque}</p>
+        </br>
+        `
+        /* return "Nome do produto: " + this.nome + ", Descrição: " + this.descricao + this.imagemDestaque  */
     }
 }
 
-const produto = new Produto();
+const produto = new Produto("Textos Cruéis Demais", 121206, "Textos para tocar cicatrizes", 30);
 console.log(produto);
 
-const produtoDestaque = new produtoDestaque();
+const produtoDestaque = new ProdutoDestaque("TOG", 121022, "A lâmina da assassina", 42, "https://m.media-amazon.com/images/I/81UKoa+LByL.jpg");
 console.log(produtoDestaque);
+
+var destaque = document.getElementById("produto-destaque");
+destaque.insertAdjacentHTML('afterbegin', produtoDestaque.mostrarProdutoDestaque());
+
+var prod = document.getElementById("produto");
+prod.insertAdjacentHTML('afterbegin', produto.mostrarProduto());
