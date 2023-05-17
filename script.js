3fd9358cd85788108304f7458bd49892d060599a
valencsoares <pvcampossoares558@gmail.com>
2023-05-17 17:37:47 -0400

    Criado novos produtos, add img no ProdutoDestaque

--- script.js
+++ script.js
@@ -28,21 +28,48 @@ class ProdutoDestaque extends Produto {
         <p>${this.datadeCadastro}</p>
         <p>${this.descricao}</p>
         <p>${this.preco}</p>
-        <p>${this.imagemDestaque}</p>
+        <img src="${this.imagemDestaque}" width="200"></img>
         </br>
         `
         /* return "Nome do produto: " + this.nome + ", Descrição: " + this.descricao + this.imagemDestaque  */
     }
 }
 
+/* class Listaprodutos extends ProdutoDestaque {
+    constructor(nome, datadeCadastro, descricao, preco, imagemDestaque){
+        super(nome, datadeCadastro, descricao, preco, imagemDestaque)
+    }
+    mostrarListaDeProdutos(){
+        return `
+        <p>${this.Produto()}</p>
+        <p>${this.ProdutoDestaque()}</p>
+        </br>
+        `
+    }
+} */
+
 const produto = new Produto("Textos Cruéis Demais", 121206, "Textos para tocar cicatrizes", 30);
+console.log(produto);
+
+const produto2 = new Produto("Textos Cruéis Demais", 121206, "Textos para tocar cicatrizes", 30);
+console.log(produto);
+
+const produto3 = new Produto("Textos Cruéis Demais", 121206, "Textos para tocar cicatrizes", 30);
 console.log(produto);
 
 const produtoDestaque = new ProdutoDestaque("TOG", 121022, "A lâmina da assassina", 42, "https://m.media-amazon.com/images/I/81UKoa+LByL.jpg");
 console.log(produtoDestaque);
+
+/* const listaProdutos = new Listaprodutos("TOG", 121022, "A lâmina da assassina", 42, "https://m.media-amazon.com/images/I/81UKoa+LByL.jpg");
+console.log(listaProdutos); */
 
 var destaque = document.getElementById("produto-destaque");
 destaque.insertAdjacentHTML('afterbegin', produtoDestaque.mostrarProdutoDestaque());
 
 var prod = document.getElementById("produto");
 prod.insertAdjacentHTML('afterbegin', produto.mostrarProduto());
+prod.insertAdjacentHTML('afterbegin', produto2.mostrarProduto());
+prod.insertAdjacentHTML('afterbegin', produto3.mostrarProduto());
+
+/* var lista = document.getElementById("lista-produtos");
+lista.insertAdjacentHTML('afterbegin', produto.mostrarListaDeProdutos()); */
